289faaf4eeab89330039faceb692efb793621c35
yangkang998 <85793754+yangkang998@users.noreply.github.com>
2021-06-15 10:42:01 +0800

    Update plotter.sh

--- plotter.sh
+++ plotter.sh
@@ -4,11 +4,11 @@ prefix="/mnt/farm"
 #你的HDD数量，挂载点标号需从1开始标号。如 /mnt/farm1, /mnt/farm2 ....
 totalDisk=6
 #你的vCPU数量
-threads=40
+threads=48
 #你的农民公钥
-fKey=""
+fKey="b09289509b32255d876b67428d7444e80a461395329a002ce127ba7482bc5fc9291b929aee5cf0b4c1f7d917988e9eb4"
 #你的矿池公钥
-pKey=""
+pKey="aef76eece268a5b076aa906b17769529a8831f2431c022a18c0aad92c5a7a9bd25dff436b472b4dd81943500425e351f"
 #第一缓存目录，一定要以/结尾
 tmp1="/mnt/plot/"
 #第二缓存目录，可与第一个相同
